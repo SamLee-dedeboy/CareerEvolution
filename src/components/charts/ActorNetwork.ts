623ebb7d730e98331b80a56010965f8db5fe88a8
SamLee-dedeboy <583637704@qq.com>
2022-11-14 10:15:09 -0800

    Add template structure for components & charts

---
+++ src/components/charts/ActorNetwork.ts
@@ -0,0 +1,17 @@
+import * as d3 from "d3"
+export class ActorNetwork {
+    svg: any;
+    public constructor(svgSelector) {
+        this.svg = d3.select(svgSelector)
+
+    }
+
+    init() {
+
+    }
+
+    update() {
+
+    }
+
+}
--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="homeview-container">
         <p> Home view</p>
+        <ActorNetwork></ActorNetwork>
     </div>
 </template>
+<script setup lang="ts">
+import ActorNetwork from '../components/ActorNetwork.vue';
+</script>
