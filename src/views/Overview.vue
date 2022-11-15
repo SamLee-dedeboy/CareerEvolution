0eaa382a61103a1ad9329246a9d70e886548a3f0
SamLee-dedeboy <583637704@qq.com>
2022-11-14 21:06:04 -0800

    add template code for accessing store & backend

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -9,6 +9,26 @@ import ActorNetwork from '../components/ActorNetwork.vue';
 import { useStore } from 'vuex'
 import * as vue from "vue"
 const store = useStore()
+
+// test store
 const test_store = vue.computed(() => store.state.test)
 console.log(test_store.value)
+
+// test backend request
+const server_address = vue.inject("server_address")
+await fetch(`${server_address}/data/artists`, 
+    // {
+    //     method: "POST",
+    //     headers: {
+    //         "Accept": "application/json",
+    //         "Content-Type": "application/json"
+    //     },
+    // }
+    )
+    .then(res => res.json())
+    .then(json => {
+        console.log(json)
+    })
+
+
 </script>
