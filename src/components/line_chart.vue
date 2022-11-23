f5df6bec51d8745ce1f7c5959e5284bfe5f3897b
hylu1994 <hyllu@ucdavis.edu>
2022-11-23 15:10:32 -0800

    In vue data preprocess to coordinates.

--- src/components/line_chart.vue
+++ src/components/line_chart.vue
@@ -31,13 +31,53 @@
         },
         mounted(){ // actually drawing
             console.log("In linechart: ", this.myData);
-            // let localData = testData['data'];
-            // this.prepareData(localData);
+            this.prepareData(this.myData);
             // this.drawLineChart(localData, "#lines");
         },
         methods: {
             prepareData(data) {
-                
+                let group_offset = {"0": 5, "1": 55, "2": 105, "3": 155, "4": 205, "5": 255};
+                let group_counter = {"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0};
+
+                // rank the initial order for all ppl.
+                let ppl_order = [];
+                Object.keys(data).forEach(key => {
+                    let tmp = {
+                        bin: data[key]['pop_bin'][0],
+                        id: key
+                    };
+                    ppl_order.push(tmp)
+                });
+                ppl_order.sort(function(a, b){
+                    if(a.bin < b.bin) { return -1; }
+                    if(a.bin > b.bin) { return 1; }
+                    return 0;
+                });
+                console.log(ppl_order);
+
+                // create line coordinates for all ppl in the subset.
+                let ppl_lines = [];
+                ppl_order.forEach(ppl => {
+                    let ppl_line = [];
+                    let bin_used = {};
+                    for (let idx=0; idx<data[ppl.id]['pop_bin'].length; idx++){
+                        let ppl_year = {
+                            id: ppl.id,
+                            year: idx,
+                            bin: group_offset[String(data[ppl.id]['pop_bin'][idx])] + 2*group_counter[String(data[ppl.id]['pop_bin'][idx])],
+                        };
+                        
+                        bin_used[String(data[ppl.id]['pop_bin'][idx])] = "true";
+                        ppl_line.push(ppl_year);
+                    }
+                    for (let idx=0; idx<=5; idx++){
+                        if (idx in bin_used){
+                            group_counter[idx] += 1;
+                        }
+                    }
+                    ppl_lines.push(ppl_line);
+                });
+                console.log(ppl_lines);
             },
             drawLineChart(data, id) {
 
