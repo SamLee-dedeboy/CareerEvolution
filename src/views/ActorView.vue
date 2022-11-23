c835f5f2c9643343036d6248cfb544a7c7eec69e
SamLee-dedeboy <samytleezzz@gmail.com>
2022-11-22 16:03:12 -0800

    add basic request to access data from backend

--- src/views/ActorView.vue
+++ src/views/ActorView.vue
@@ -6,6 +6,36 @@
 
 <script setup lang="ts">
 import Timeline from '../components/Timeline.vue';
+import * as vue from "vue"
+import { Ref, ref } from "vue"
+
+const server_address = vue.inject("server_address")
+const artists: Ref<any[]> = ref([])
+const target_artist: Ref<any> = ref(undefined)
+const target_artist_career: Ref<any> = ref(undefined)
+vue.onMounted(() => {
+    get_artists()
+    target_artist.value = 'nm0000375'
+    get_career(target_artist.value)
+})
+
+async function get_career(artist) {
+    await fetch(`${server_address}/data/career/${artist}`)
+        .then(res => res.json())
+        .then(json => {
+            target_artist_career.value = json
+            console.log("career fetched", json)
+        })
+}
+
+async function get_artists() {
+    await fetch(`${server_address}/data/artists`)
+        .then(res => res.json())
+        .then(json => {
+            artists.value = json
+            console.log("artists fetched", json)
+        })
+}
 
 const test_data = [
     { // section
