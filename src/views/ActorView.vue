759cc1aba5f1fbe16c3eec00e1fad728bed5596d
SamLee-dedeboy <583637704@qq.com>
2022-11-23 15:23:06 -0800

    add stage header & descriptions

--- src/views/ActorView.vue
+++ src/views/ActorView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="actor-view-container">
-        <Timeline :timeline_data="target_artist_career" :section_desciptions="test_descriptions"></Timeline>
+        <Timeline :timeline_data="target_artist_career" :section_desciptions="stage_descriptions"></Timeline>
     </div>
 </template>
 
@@ -13,7 +13,7 @@ const server_address = vue.inject("server_address")
 const artists: Ref<any[]> = ref([])
 const target_artist: Ref<any> = ref(undefined)
 const target_artist_career: Ref<any> = ref(undefined)
-const target_artist_career_data: Ref<any> = ref(undefined)
+const stage_descriptions: Ref<any> = ref(undefined)
 vue.onMounted(() => {
     get_artists()
     target_artist.value = 'nm0000375'
@@ -25,6 +25,12 @@ async function get_career(artist) {
         .then(res => res.json())
         .then(json => {
             target_artist_career.value = json
+            stage_descriptions.value = json.map(stage => {
+                return {
+                    "header": stage.header,
+                    "description": stage.paragraphs
+                }
+            })
             console.log("career fetched", json)
         })
 }
@@ -37,39 +43,5 @@ async function get_artists() {
             console.log("artists fetched", json)
         })
 }
-
-const test_data = [
-    { // section
-        header: "stage 1",
-        movies: [0, 0, 0, 0],
-    },
-    { // section
-        header: "stage 2",
-        movies: [1, 1, 1, 1],
-    },
-    { // section
-        header: "stage 3",
-        movies: [2, 2, 2, 2],
-    },
-    { // section
-        header: "stage 3",
-        movies: [3, 3, 3, 3],
-    },
-]
-
-const test_descriptions = [
-    {
-        header: "Early Stage",
-        description: "At his early stage of career, he was someweqewqewqewqewqewqewqewqewq"
-    },
-    {
-        header: "Middle Stage",
-        description: "At his middle stage of career, he was someweqewqewqewqewqewqewqewqewq"
-    },
-    {
-        header: "Late Stage",
-        description: "At his late stage of career, he was someweqewqewqewqewqewqewqewqewq"
-    },
-]
 
 </script>
