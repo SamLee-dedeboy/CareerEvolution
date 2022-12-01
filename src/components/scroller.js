9e1b5be796c16bd0220090c19eeb184ba918d375
SamLee-dedeboy <samytleezzz@gmail.com>
2022-12-01 15:15:09 -0800

    change active position

--- src/components/scroller.js
+++ src/components/scroller.js
@@ -97,7 +97,7 @@ var scroller = {
      */
     function position() {
       // var pos = window.pageYOffset - 10 - containerStart;
-      var pos = window.pageYOffset - 10 - containerStart;
+      var pos = window.pageYOffset + 100 - containerStart;
       var sectionIndex = d3.bisect(sectionPositions, pos);
       sectionIndex = Math.min(sections.size() - 1, sectionIndex);
 
